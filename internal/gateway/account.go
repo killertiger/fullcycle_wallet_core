fea41605cf63971acce9e737d5b0152eefa445bd
moliveira <moliveira@pinterest.com>
2024-02-27 16:52:36 -0800

    feat: :sparkles: CreateAccountUseCase

---
+++ internal/gateway/account.go
@@ -0,0 +1,8 @@
+package gateway
+
+import "github.com/killertiger/fullcycle_wallet_core/internal/entity"
+
+type AccountGateway interface {
+	Save(account *entity.Account) error
+	FindByID(id string) (*entity.Account, error)
+}
