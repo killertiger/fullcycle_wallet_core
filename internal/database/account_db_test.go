e28d28f14353ecf336ab5080e49863f6bc4b260d
moliveira <moliveira@pinterest.com>
2024-02-28 02:44:08 -0800

    feat: :sparkles: AccountDB

---
+++ internal/database/account_db_test.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/killertiger/fullcycle_wallet_core/internal/entity"
+	"github.com/stretchr/testify/suite"
+)
+
+type AccountDBTestSuite struct {
+	suite.Suite
+	db        *sql.DB
+	accountDB *AccountDB
+	client    *entity.Client
+}
+
+func (s *AccountDBTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	s.Nil(err)
+	s.db = db
+	db.Exec("CREATE TABLE clients (id varchar(255), name varchar(255), email varchar(255), created_at date)")
+	db.Exec("CREATE TABLE accounts (id varchar(255), client_id varchar(255), balance int, created_at date)")
+	s.accountDB = NewAccountDB(db)
+	s.client, _ = entity.NewClient("John", "j@j.com")
+}
+
+func (s *AccountDBTestSuite) TearDownSuite() {
+	defer s.db.Close()
+	s.db.Exec("DROP TABLE clients")
+	s.db.Exec("DROP TABLE accounts")
+}
+
+func TestAccountDBTestSuite(t *testing.T) {
+	suite.Run(t, new(AccountDBTestSuite))
+}
