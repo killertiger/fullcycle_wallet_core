885ddc7e499c78f06ece1620842858200fe9423d
moliveira <moliveira@pinterest.com>
2024-03-07 16:15:48 -0800

    removing mysql files

--- cmd/walletcore/main.go
+++ cmd/walletcore/main.go
@@ -14,7 +14,8 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=true&loc=Local", "root", "root", "localhost", "3306", "wallet"))
+	// db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=true&loc=Local", "root", "root", "localhost", "3306", "wallet"))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?&parseTime=true&loc=Local", "root", "root", "localhost", "3306", "wallet"))
 	if err != nil {
 		panic(err)
 	}
