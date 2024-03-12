e217f161fd3c33817ea9793dadcbb501b6bb748e
moliveira <moliveira@pinterest.com>
2024-03-12 05:17:44 -0700

    feat: :sparkles: Update account balance on creating transaction

--- internal/gateway/account.go
+++ internal/gateway/account.go
@@ -5,4 +5,5 @@ import "github.com/killertiger/fullcycle_wallet_core/internal/entity"
 type AccountGateway interface {
 	Save(account *entity.Account) error
 	FindByID(id string) (*entity.Account, error)
+	UpdateBalance(account *entity.Account) error
 }
